539893c1de7dfc3bd07797cdd2fb6e2144ac8813
congminh090800 <congminh090800@gmail.com>
2020-11-23 20:29:49 +0700

    profile layout

--- app/src/main/java/com/example/goeat/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/goeat/Fragments/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,10 +17,18 @@ import com.example.goeat.auth.Auth;
 
 import org.apache.commons.lang3.ObjectUtils;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class ProfileFragment extends Fragment {
 
     Button logout;
+    Button changepassword;
     Auth mAuth;
+    TextView username;
+    TextView email;
+    TextView gender;
+    TextView date;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,21 @@ public class ProfileFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
         mAuth = Auth.getInstance();
         User u = mAuth.getCurrentUser();
+        username = v.findViewById(R.id.username);
+        username.append(u.getUsername());
+        email = v.findViewById(R.id.email);
+        email.append(u.getEmail());
+        gender = v.findViewById(R.id.gender);
+        gender.append(u.getGender());
+        date = v.findViewById(R.id.date);
+        date.append(new Date(TimeUnit.DAYS.toDays(u.getDateCreated()))+"");
+
+        changepassword = v.findViewById(R.id.logout);
+        changepassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
         logout = v.findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
